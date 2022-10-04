a4c9db0ce7261808a6b556fe33bae252b3ee1d6a
marko <markospyros1@gmail.com>
2022-10-04 18:46:24 +0200

    fetched json file from nettskjema

--- src/Screens/FormScreen.js
+++ src/Screens/FormScreen.js
@@ -1,45 +1,30 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { Text, View, DevSettings } from "react-native";
-import ForwardButton from "../components/FormScreenComponents/ForwardButton/ForwardButton";
-import Question from "../components/FormScreenComponents/Question/Question";
-import { questionaires } from "../components/FormScreenComponents/Question/Questions";
-import RadioButtonGroup from "../components/FormScreenComponents/RadioButton/RadioButtonGroup";
-import { appContainer } from "../components/FormScreenComponents/style";
+import { Text, View, DevSettings, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import ForwardButtonGroup from "../components/FormScreenComponents/ForwardButton/ForwardButtonGroup";
+import Question from "../components/FormScreenComponents/Question/Question";
+import { questionnaires } from "../components/FormScreenComponents/Question/Questions";
 
-const FormScreen = ({ route, navigation }) => {
-  const questions = questionaires.map((question) => question.question);
+const FormScreen = () => {
+  const questions = questionnaires.map((question) => question.text);
 
-  const categories = questionaires.map((question) => question.category);
+  const answerOptions = questionnaires.map((option) => option.answerOptions);
+
+  console.log(answerOptions[0].text);
 
   let [counter, setCounter] = useState(0);
 
   let [question, setQuestion] = useState(questions[counter]);
 
-  let [category, setCategory] = useState(categories[counter]);
-
-  let [selectedId, setSelectedId] = useState();
-
   const onNext = () => {
-    if (counter < questions.length - 1) {
-      setCounter((counter += 1));
-      setQuestion(questions[counter]);
-      setCategory(categories[counter]);
-    }
+    setCounter((counter += 1));
+    setQuestion(questions[counter]);
+    console.log(question);
   };
 
   return (
-    <SafeAreaView style={appContainer}>
-      <StatusBar />
+    <SafeAreaView>
       <Question question={question} />
-      <RadioButtonGroup category={category} selectedId={selectedId} />
-      <ForwardButtonGroup
-        onNext={onNext}
-        counter={counter}
-        limit={questions.length - 1}
-      />
     </SafeAreaView>
   );
 };
